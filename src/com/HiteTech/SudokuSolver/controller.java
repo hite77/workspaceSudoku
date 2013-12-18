68d60e77313522438442eb06beef6535e1218b68
Jason Hite <hite77@gmail.com>
2013-12-17 22:59:15 -0500

    starting with some code

--- src/com/HiteTech/SudokuSolver/controller.java
+++ src/com/HiteTech/SudokuSolver/controller.java
@@ -1,5 +1,32 @@
 package com.HiteTech.SudokuSolver;
 
+import java.util.Vector;
+
 public class controller {
 
+	private Vector<board> Boards = new Vector<board>();
+	
+	public controller() {
+		board Board = new board();
+		Boards.add(Board);
+	}
+	
+	public board GetBoard() {
+		return Boards.get(0);
+	}
+
+	public int Count() {
+		return Boards.size();
+	}
+
+	public void Guess(int i, int j) {
+		// research way to keep several boards
+		// need to copy from where cursor is at and make N number (N<=9) boards
+		// place values into each one.
+		for (int easy=0; easy <8; easy++)
+		{
+			board Board = new board();
+			Boards.add(0, Board);
+		}
+	}
 }
